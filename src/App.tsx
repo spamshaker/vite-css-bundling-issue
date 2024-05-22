b6cac1beb197910ad50006a5497ae211e15f492f
Michał Grzegorzewski <michal@ria-develop.pl>
2024-05-22 06:45:56 +0200

    Add theme loaders for light and dark themes
    Two new theme loaders have been added: glob-based and switch-based, both capable of loading either the light or dark theme. These loaders return the respective theme CSS as a promise, as declared in their type definitions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { loadTheme } from './theme-loader.ts';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { loadTheme } from 'themes/loader';
 
 await loadTheme(new URLSearchParams(location.search).get('theme') || 'light');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -32,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/theme-loader.ts
+++
@@ -1,8 +0,0 @@
-const glob = import.meta.glob('./themes/*');
-
-export async function loadTheme(name: string) {
-  const theme = `./themes/theme-${name}.css`;
-  await glob[theme]();
-  // The following not working
-  // await import(theme, { with: { type: 'css' } });
-}
